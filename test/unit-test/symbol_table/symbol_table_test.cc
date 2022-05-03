e5848f3e1029a6c2856d2c8d6bc14981c4012310
chujDK <1768485949@qq.com>
2022-05-03 22:17:23 +0800

    chore: update cmakefile

--- test/unit-test/symbol_table/symbol_table_test.cc
+++ test/unit-test/symbol_table/symbol_table_test.cc
@@ -9,15 +9,55 @@
 // 8. get a symbol's memory (which means its inital value)
 #include <gtest/gtest.h>
 
+#include <cstring>
+#include <memory>
+
 #include "syparse.h"
 #include "sysymbol_table.h"
 
 TEST(SymbolTableTest, ident_test) {
     SymbolTableAPI* symbol_table = (SymbolTableAPI*)new SymbolTable();
 
-    auto ident = AstNodePool::get(SyAstType::IDENT, 0, "a");
+    auto ident = AstNodePool::get(SyAstType::IDENT, 0, "a");  // int a;
     symbol_table->addSymbol(ident->getLiteral(), SyAstType::VAL_TYPE_INT,
                             nullptr, 0, false);
+    EXPECT_NE(symbol_table->searchTable("a"), nullptr);         // searched
+    EXPECT_NE(symbol_table->searchCurrentScope("a"), nullptr);  // searched
+    EXPECT_EQ(symbol_table->searchTable("a")->getType(),
+              SyAstType::VAL_TYPE_INT);
+    EXPECT_EQ(symbol_table->searchCurrentScope("a")->getMem(),
+              nullptr);  // no init val
+
+    EXPECT_EQ(symbol_table->searchTable("b"), nullptr);  // no searched
+    ident      = AstNodePool::get(SyAstType::IDENT, 0, "b");
+    Value* val = new Value();
+    val->i32   = 0xDEADBEEF;  // int b = 0xDEADBEEF;
+    symbol_table->enterScope();
+    symbol_table->addSymbol(ident->getLiteral(), SyAstType::VAL_TYPE_INT, val,
+                            0, false);
+    // remember this
+    val = nullptr;
+
+    EXPECT_NE(symbol_table->searchTable("b"), nullptr);  // searched
+    EXPECT_EQ(symbol_table->searchTable("b")->getType(),
+              SyAstType::VAL_TYPE_INT);
+    ASSERT_NE(symbol_table->searchTable("b")->getMem(), nullptr);
+    EXPECT_EQ(symbol_table->searchTable("b")->getMem()->i32, 0xDEADBEEF);
+
+    ident = AstNodePool::get(SyAstType::IDENT, 0, "c");
+    val   = new Value[0x100];
+    memset(val, 0, 0x100 * sizeof(Value));
+    val[0].i32     = 0xDEADBEEF;
+    val[0x100].i32 = 0x13377331;  // int c[0x10][0x10]; c[0][0] = 0xDEADBEEF;
+                                  // c[0xF][0xF] = 0x13377331;
+    symbol_table->addGlobalSymbol(ident->getLiteral(),
+                                  SyAstType::VAL_TYPE_INT_ARRAY, val, 0x100, 0);
+
+    ArrayMemoryPtr array_c = std::dynamic_pointer_cast<ArrayMemoryAPI>(
+        symbol_table->searchTable("c"));
+    val = nullptr;
+
+    ASSERT_NE(array_c, nullptr);
 }
 
 // a function table should be able:
